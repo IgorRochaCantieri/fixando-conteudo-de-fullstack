5ae7bbda43b4c2af3142df270c71b911b3d1bac1
Igor Rocha Cantieri <igorrocha2198@gmail.com>
2026-08-28 13:43:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function displayRadioValue(){
-    let elementos = document.getElementsByName('gender');
-
-    for(var i=0; i<elementos.length; i++){
-        if(elementos[i].checked){
-            document.getElementById("resultado").innerHTML = "Gênero: " + elementos[i].value;
-        }
-    }
-}
