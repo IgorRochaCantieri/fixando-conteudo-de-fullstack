425095fc71bd4a90492da1cc5451f2e0559d20aa
Igor Rocha Cantieri <igorrocha2198@gmail.com>
2026-08-30 09:30:30 -0300

    Update script.js

--- ex003/script.js
+++ ex003/script.js
@@ -1,10 +1,18 @@
 function getChekedBox(){
     //Pegue todos os inputs que são do tipo checkbox e que estão marcados
     let prioridades = document.querySelectorAll('input[type="checkbox"]:checked');
+    // Treinando objeto DOM ==  o document.querySelector('input') pega primeiro input
+    let objetoInput = document.querySelector('input');
+
     let text = "";
     for(var i=0; i<prioridades.length; i++){
         text = `${text}, ${prioridades[i].value}`;
     }
 
     document.getElementById('prioridades').textContent = text;
+
+    console.log(objetoInput.name);
+    console.log(objetoInput.value);
+    console.log(objetoInput.type);
+    console.log(objetoInput.checked);
 }
